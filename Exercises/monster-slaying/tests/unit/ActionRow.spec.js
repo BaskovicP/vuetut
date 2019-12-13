51dae60908bd574fad869193fec24dfcd639fa04
BaskovicP <pbaskovi@gmail.com>
2019-12-13 17:35:05 +0100

    Add the tests for the dynamic background in actionrow class

--- Exercises/monster-slaying/tests/unit/ActionRow.spec.js
+++ Exercises/monster-slaying/tests/unit/ActionRow.spec.js
@@ -11,4 +11,35 @@ describe.only("ActionRow.vue", () => {
   it("should contain vue instance", () => {
     expect(prepareWrapper().isVueInstance()).toEqual(true);
   });
+
+  it("should have a red class and monster hits player text and class red", () => {
+    let wrapper = prepareWrapper({
+      subject: "MONSTER",
+      acusativus: "PLAYER",
+      demage: 10
+    });
+    expect(wrapper.find("li").text()).toEqual("MONSTER HITS PLAYER FOR 10");
+    expect(wrapper.find("li").classes()).toContain("red");
+  });
+
+  it(
+    "should have a red class and monster hits player text and class red" +
+      "and blue class if player hits monster",
+    () => {
+      let wrapper = prepareWrapper({
+        subject: "MONSTER",
+        acusativus: "PLAYER",
+        demage: 10
+      });
+      expect(wrapper.find("li").text()).toEqual("MONSTER HITS PLAYER FOR 10");
+      expect(wrapper.find("li").classes()).toContain("red");
+      wrapper.setProps({
+        subject: "PLAYER",
+        acusativus: "MONSTER",
+        demage: 10
+      });
+      expect(wrapper.find("li").classes()).toContain("blue");
+      expect(wrapper.find("li").text()).toEqual("PLAYER HITS MONSTER FOR 10");
+    }
+  );
 });
