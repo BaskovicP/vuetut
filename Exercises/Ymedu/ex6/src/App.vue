05674e494a243b03ec362483616a69ec20e13508
BaskovicP <pbaskovi@gmail.com>
2019-12-17 12:02:24 +0100

    distribute code into components

--- Exercises/Ymedu/ex6/src/App.vue
+++ Exercises/Ymedu/ex6/src/App.vue
@@ -1,41 +1,24 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <header>
-                    <h1>Server Status</h1>
-                </header>
-            </div>
-        </div>
-        <hr>
-        <div class="row">
-            <div class="col-xs-12 col-sm-6">
-                <ul class="list-group">
-                    <li
-                            class="list-group-item"
-                            v-for="index in 5">
-                        Server #{{ index }}
-                    </li>
-                </ul>
-            </div>
-            <div class="col-xs-12 col-sm-6">
-                <p>Server Details are currently not updated</p>
-            </div>
-        </div>
-        <hr>
-        <div class="row">
-            <div class="col-xs-12">
-                <footer>
-                    <p>All Servers are managed here</p>
-                </footer>
-            </div>
-        </div>
+  <div class="container">
+    <!-- HEADER -->
+    <hr />
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <!-- LIST -->
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <!-- Server details -->
+      </div>
     </div>
+    <hr />
+    <div class="row">
+      <div class="col-xs-12">
+        <!-- Server management -->
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-</script>
+<script></script>
 
-<style>
-
-</style>
+<style></style>
