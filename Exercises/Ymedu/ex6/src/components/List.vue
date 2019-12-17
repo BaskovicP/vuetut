cc34f7401b64bf6d6c89294a0eafb119bddf4e96
BaskovicP <pbaskovi@gmail.com>
2019-12-17 12:14:22 +0100

    Add a key for the for loop

--- Exercises/Ymedu/ex6/src/components/List.vue
+++ Exercises/Ymedu/ex6/src/components/List.vue
@@ -1,6 +1,8 @@
 <template>
   <ul class="list-group">
-    <li class="list-group-item" v-for="index in 5">Server #{{ index }}</li>
+    <li class="list-group-item" v-for="index in 5" :key="index + '.key'">
+      Server #{{ index }}
+    </li>
   </ul>
 </template>
 <script>
