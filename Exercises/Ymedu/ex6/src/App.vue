29fcf37e61f3befbff6492a69bdd59fc32d7f052
BaskovicP <pbaskovi@gmail.com>
2019-12-17 12:14:31 +0100

    register components locally

--- Exercises/Ymedu/ex6/src/App.vue
+++ Exercises/Ymedu/ex6/src/App.vue
@@ -1,24 +1,38 @@
 <template>
   <div class="container">
-    <!-- HEADER -->
+    <Header></Header>
     <hr />
     <div class="row">
       <div class="col-xs-12 col-sm-6">
-        <!-- LIST -->
+        <List></List>
       </div>
       <div class="col-xs-12 col-sm-6">
-        <!-- Server details -->
+        <Details></Details>
       </div>
     </div>
     <hr />
     <div class="row">
       <div class="col-xs-12">
-        <!-- Server management -->
+        <ServerManagement></ServerManagement>
       </div>
     </div>
   </div>
 </template>
 
-<script></script>
+<script>
+//Localy registered components later used in the main app components
+import Details from "./components/Details.vue";
+import Header from "./components/Header.vue";
+import List from "./components/List.vue";
+import ServerManagement from "./components/ServerManagement.vue";
+export default {
+  components: {
+    Details,
+    Header,
+    List,
+    ServerManagement
+  }
+};
+</script>
 
 <style></style>
