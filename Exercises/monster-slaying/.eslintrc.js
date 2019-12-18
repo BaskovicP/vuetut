70ec118168fc67812b425526e3a8b489cf434ee5
BaskovicP <pbaskovi@gmail.com>
2019-12-18 11:09:44 +0100

    change eslint configuration to support module

--- Exercises/monster-slaying/.eslintrc.js
+++ Exercises/monster-slaying/.eslintrc.js
@@ -1,26 +1,11 @@
+
 module.exports = {
   root: true,
+  parserOptions: {
+    sourceType: 'module'
+  },
   extends: '@extensionengine',
   env: {
-    node: true
-  },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  },
-  parserOptions: {
-    parser: "babel-eslint"
-  },
-  overrides: [
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
+    jest: true
+  }
 };
