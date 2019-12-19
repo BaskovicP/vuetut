a0d1b25d68c16944f395f49a801fb989f811e286
BaskovicP <pbaskovi@gmail.com>
2019-12-19 14:47:03 +0100

    set up project with lint and tests

--- Exercises/quote-master/.eslintrc.js
+++ Exercises/quote-master/.eslintrc.js
@@ -1,28 +1,10 @@
 module.exports = {
   root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
+  extends: '@extensionengine',
   parserOptions: {
-    parser: 'babel-eslint'
+    sourceType: 'module'
   },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
-}
+  env: {
+    jest: true
+  }
+};
--- Exercises/quote-master/tests/unit/example.spec.js
+++ Exercises/quote-master/tests/unit/example.spec.js
@@ -1,12 +1,14 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mount } from '@vue/test-utils';
+import ProgressBar from '@/components/ProgressBar';
 
 describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
-})
+  const prepareWrapper = props => {
+    return mount(ProgressBar, {
+      propsData: props
+    });
+  };
+  it('exists', () => {
+    const wrapper = prepareWrapper();
+    expect(wrapper.isVueInstance()).toBe(true);
+  });
+});
