7885ab6dd2c6ca001d05e2b8a7fb6e9a5308e071
BaskovicP <pbaskovi@gmail.com>
2019-12-21 19:35:09 +0100

    create single quote component

--- Exercises/quote-master/src/components/SingleQuote.vue
+++ Exercises/quote-master/src/components/SingleQuote.vue
@@ -1,19 +1,18 @@
 <template>
-  <div class="row container">
-    <div id="main-text" class="column-xl-8">
-      {{ quoteText }}
-    </div>
+  <div id="main-text" class="column-xl-8">
+    <slot></slot>
   </div>
 </template>
 
 <script>
 export default {
-  props: {
-    quoteText: { type: String, required: true }
-  }
 };
 </script>
 
 <style>
-
+#main-text {
+  font-family: cursive;
+  border: 1px solid black;
+  padding: 0.5rem;
+}
 </style>
