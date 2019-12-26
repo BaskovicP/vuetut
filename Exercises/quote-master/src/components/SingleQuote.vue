3cb7ac1ea3aeb438a466af3bcf73ae5a144b422f
BaskovicP <pbaskovi@gmail.com>
2019-12-26 10:37:46 +0100

    add delete event on click

--- Exercises/quote-master/src/components/SingleQuote.vue
+++ Exercises/quote-master/src/components/SingleQuote.vue
@@ -1,16 +1,24 @@
 <template>
-  <div id="main-text" class="column-xl-8">
+  <div @click="sendDeleteEvent" class="column-xl-8 main-text">
     <slot></slot>
   </div>
 </template>
 
 <script>
 export default {
+  props: {
+    id: { type: Number, required: true }
+  },
+  methods: {
+    sendDeleteEvent() {
+      this.$emit('delete', this.id);
+    }
+  }
 };
 </script>
 
 <style>
-#main-text {
+.main-text {
   font-family: cursive;
   border: 1px solid black;
   padding: 0.5rem;
