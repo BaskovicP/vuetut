31b0629fbb6f491c420b4caf1ec5d50406793246
BaskovicP <pbaskovi@gmail.com>
2019-12-30 20:30:22 +0100

    add testing of button clicking form

--- Exercises/Ymedu/forms/specs/App.spec.js
+++ Exercises/Ymedu/forms/specs/App.spec.js
@@ -1,13 +1,26 @@
-import MainApp from "@/App"
-import {mount} from '@vue/test-utils'
-describe('App.vue', () => {
-    const prepareWrapper= props => mount(MainApp,{ propsData: props})
-    let wrapper = prepareWrapper();
+import MainApp from "@/App";
+import { mount } from "@vue/test-utils";
+describe("App.vue", () => {
+  const prepareWrapper = props => mount(MainApp, { propsData: props });
+  let wrapper = prepareWrapper();
 
-    beforeEach(()=> {
-     wrapper = prepareWrapper();
-    })
-    it('should be defined', () => {
-        expect(wrapper.isVueInstance()).toBe(true);
-    });
+  beforeEach(() => {
+    wrapper = prepareWrapper();
+  });
+  it("should be defined", () => {
+    expect(wrapper.isVueInstance()).toBe(true);
+  });
+  it("should have the right info", async () => {
+    wrapper.find("#first-name").setValue("Paulo");
+    wrapper.find("#last-name").setValue("Legenda");
+    wrapper.find("#mail").setValue("paulo@");
+    wrapper.find("#password").setValue("passwor1");
+    await wrapper.vm.$nextTick();
+    expect(wrapper.text()).not.toMatch("paulo@");
+    expect(wrapper.text()).not.toMatch("passwor1");
+    wrapper.find("#main-submit-button").trigger("click");
+    await wrapper.vm.$nextTick();
+    expect(wrapper.text()).toMatch("paulo@");
+    expect(wrapper.text()).toMatch("passwor1");
+  });
 });
