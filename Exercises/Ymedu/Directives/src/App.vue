7719f98d443fa91ee7bf49008ac44d990a76df1c
BaskovicP <pbaskovi@gmail.com>
2020-01-08 16:29:45 +0100

    preparation for the new exercise

--- Exercises/Ymedu/Directives/src/App.vue
+++ Exercises/Ymedu/Directives/src/App.vue
@@ -1,36 +1,29 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1 v-myon:click="consoleLog('Don\'t you click on me!!!!')">
-          Directives Exercise
-        </h1>
-        <h1 v-myon:mouseover="consoleLog('PLease do not touch me!!!')">
-          Directives Exercise moody
-        </h1>
-        <!-- Exercise -->
-        <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
-      </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Filters & Mixins</h1>
+                <!-- Exercise 1) -->
+                <!-- Build a local Filter which reverses the Text it is applied on -->
+
+                <!-- Exercise 2 -->
+                <!-- Build a global Filter which counts the length of a word and it appends it -->
+                <!-- Like this: "Test" => Gets Filtered to => "Test (4)" -->
+
+                <!-- Exercise 3 -->
+                <!-- Do the same as in Exercises 1 & 2, now with Computed Properties -->
+
+                <!-- Exercise 4 -->
+                <!-- Share the Computed Property rebuilding Exercise 2 via a Mixin -->
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-export default {
-  directives: {
-    myon: {
-      bind(el, binding, vnode) {
-        el.addEventListener(binding.arg, binding.value);
-      }
+    export default {
     }
-  },
-  methods: {
-      // we use this method because vue prefixes all the function calls in template with _vm
-    consoleLog(message) {
-      return () => console.log(message);
-    }
-  }
-};
 </script>
 
-<style></style>
+<style>
+</style>
