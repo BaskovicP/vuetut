bccaf62773c885a36b093fdb71853ced1b402d50
BaskovicP <pbaskovi@gmail.com>
2020-01-10 18:14:38 +0100

    add an imperative jump

--- Exercises/Ymedu/routing-project/src/App.vue
+++ Exercises/Ymedu/routing-project/src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
-    <router-link to="/user" tag="button" active-class="my-active">
+    <router-link to="/user" tag="button" active-class="my-active" exact>
       UserPage</router-link
     >
     <br />
     <br />
+    <button @click="leadMeToSomewhere">
+      Imperative jump
+    </button>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: "app"
+  name: "app",
+  methods: {
+    leadMeToSomewhere() {
+      this.$router.push("/newPage");
+    }
+  }
 };
 </script>
 
