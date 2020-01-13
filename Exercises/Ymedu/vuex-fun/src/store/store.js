db2042c5f707bcc23a8c5b8241c497981ab11e9d
BaskovicP <pbaskovi@gmail.com>
2020-01-13 17:59:23 +0100

    use rudimental getter

--- Exercises/Ymedu/vuex-fun/src/store/store.js
+++ Exercises/Ymedu/vuex-fun/src/store/store.js
@@ -6,5 +6,9 @@ Vue.use(Vuex);
 export const store = new Vuex.Store({
   state: {
     counter: 0
+  },
+  getters: {
+    // name : state => function
+    doubleCounter: state => state.counter * 2
   }
 });
