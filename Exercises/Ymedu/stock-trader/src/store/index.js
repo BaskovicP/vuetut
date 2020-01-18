3385385508df9e62b62e2e9b8b476e8fbe5374e0
BaskovicP <pbaskovi@gmail.com>
2020-01-18 13:57:45 +0100

    setting up the store

--- Exercises/Ymedu/stock-trader/src/store/index.js
+++ Exercises/Ymedu/stock-trader/src/store/index.js
@@ -1,15 +1,15 @@
+import * as actions from './actions';
+import portfolio from './modules/portfolio';
+import stocks from './modules/stocks';
 import Vue from 'vue';
 import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  actions,
   modules: {
+    stocks,
+    portfolio
   }
 });
