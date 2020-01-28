3991b3cba547f7c9a9a38f3e1c98fe86ed701853
BaskovicP <pbaskovi@gmail.com>
2020-01-28 17:12:17 +0100

    refactor long code attributes

--- Exercises/Ymedu/stock-trader/src/components/portfolio/Portfolio.vue
+++ Exercises/Ymedu/stock-trader/src/components/portfolio/Portfolio.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="row">
-    <div v-if="stocks.length<=0">Please buy some stocks, you don't want to end poor.</div>
+    <div v-if="noStocks">Please buy some stocks, you don't want to end poor.</div>
     <app-stock v-for="stock in stocks" :key="stock.stockId" :stock="stock" />
   </div>
 </template>
@@ -13,7 +13,8 @@ export default {
   computed: {
     ...mapGetters({
       stocks: 'stockPortfolio'
-    })
+    }),
+    noStocks() { return this.stocks.length <= 0; }
   },
   components: {
     appStock: Stock
